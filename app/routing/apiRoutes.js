8e600983100520ba66e03762d7be07a54d6cdce2
spartan172321 <chronger1115@gmail.com>
2017-06-29 19:25:05 -0400

    Temp

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,13 +2,16 @@
 // var holds the arrays of info the users input into the survey
 
 var friendData = require("../data/friends");
+var fs = require('fs');
 
 
 //ROUTING
 
 module.exports = function(app){
 	// code below allows user to visit the api info page
+
 	app.get("/api/friends", function(req, res){
+		console.log(friendData);
 		res.json(friendData);
 	});
 
@@ -17,8 +20,23 @@ module.exports = function(app){
 	// below code handles when the user submits the survey and the data goes to the server. The data is in the form of a JSON object and pushed into an array
 
 	app.post("/api/friends", function(req, res){
-		console.log(req.body)
-		friendData.push(req.body);
-		res.json(true);
+		var scoreArr = req.body.scores;
+    var scoreNums = [];
+    for(var i in scoreArr){
+      scoreNums.push(parseInt(scoreArr[i]));
+    }
+    var userData = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: scoreNums
+    }
+		friendData.push(userData);
+
+		// for (var i in friendData){
+
+		// }
+
+
+		res.json(friendData[0]);
 	});
 }
